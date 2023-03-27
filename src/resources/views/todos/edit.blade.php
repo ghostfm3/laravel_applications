00cf91e424a87593325f6068bbad20d88cc19a8c
ghostfm3 <mi20a003@oecu.jp>
2023-03-27 12:12:09 +0900

    first commit

--- src/resources/views/todos/edit.blade.php
+++ src/resources/views/todos/edit.blade.php
@@ -33,7 +33,9 @@
         @endif
     </div>
 </body>
+<center>
 <footer>
         <small>copyrights &copy; 2022 PHP, Laravel Studing Group All rights Reserved.</small>
         </footer>
+</center>
 </html>
